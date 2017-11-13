951e6da96d705500f09e2501527bf509a4b71bba
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2017-11-12 22:44:54 -0500

    modifying based on testing in another project
    will fit it into this better later

--- EntropyTest2/Program.cs
+++ EntropyTest2/Program.cs
@@ -133,132 +133,158 @@ namespace EntropyTest
 
 
 
+
+
 namespace GenericTools
 {
     public class Toolkit
     {
-        
+
         int Type;
-        long seed;
-        long multiplier;
-        long modulo;
-        long increment;
-        
+        double seed;
+        double multiplier;
+        double modulo;
+        double increment;
+
         bool debug;
-        List<long> waitinglist;
-        List<long> TimeList;
-        List<long> Seedlist;
+        List<double> waitinglist;
+        List<double> TimeList;
+        List<double> Seedlist;
         DateTime LastTime;
-        
-        public Toolkit(int type =0, bool Debug = false) // instance of generator is started with either in built crypto PRG or homebrewed entropy source, debug turns on console output
+
+       public Toolkit(int type = 0, bool Debug = false) // instance of generator is started with either in built crypto PRG or homebrewed entropy source, debug turns on console output
         {
-            
+
             debug = Debug;
             Type = type; // 0= internal crypto library, 1 = linear distribution, 2 = exponential distribution, 3  = entropy generated list
-            if (type >0)
+            waitinglist = new List<double>();
+            if (type > 0)
             {
-                waitinglist = new List<long>();
-                
-                Thread seedthread = new Thread(() => { seed = SeedGen(); });
-                seedthread.Start();
-                Thread.Sleep(1);
-                Thread multhread = new Thread(() => { multiplier = SeedGen(); });
-                multhread.Start();
-                Thread.Sleep(1);
-                Thread modthread = new Thread(() => { modulo = SeedGen(); });
-                modthread.Start();
-                Thread.Sleep(1);
-                Thread incthread = new Thread(() => { increment = SeedGen(); });
-                incthread.Start();
-                Thread.Sleep(1);
-                seedthread.Join();
-                multhread.Join();
-                modthread.Join();
-                incthread.Join();
-                
-            }
-            if (type ==2)
-            {
-                TimeList = new List<long>();
-                TimeList.Add(1);
-                LastTime = DateTime.Now;
-            }
-            if(type ==3)
-            {
-                Seedlist = new List<long>();
-                Seedlist.Add(seed);
-                Seedlist.Add(increment);
-                Seedlist.Add(multiplier);
-                Seedlist.Add(modulo);
-                Thread Genthread = new Thread(() => { ListGenerator(); });
-                Genthread.Start();
-            }
 
-        
-        }
 
-        void ListGenerator()
-        {
-            while(true)
-            {
-                Thread.Sleep(1);
-                if(Seedlist.Count < 300)
+                
+
+                if(type ==1)
                 {
-                    Thread[] threadarray = new Thread[50];
-                    for (int count = 0; count < 50; count++)
+                    Thread seedthread = new Thread(() => { seed = SeedGen(); });
+                    seedthread.Start();
+                    Thread.Sleep(1);
+                    Thread multhread = new Thread(() => { multiplier = SeedGen(); });
+                    multhread.Start();
+                    Thread.Sleep(1);
+                    Thread modthread = new Thread(() => { modulo = SeedGen(); });
+                    modthread.Start();
+                    Thread.Sleep(1);
+                    Thread incthread = new Thread(() => { increment = SeedGen(); });
+                    incthread.Start();
+                    Thread.Sleep(1);
+                    seedthread.Join();
+                    multhread.Join();
+                    modthread.Join();
+                    incthread.Join();
+                }
+
+                if (type == 2)
+                {
+                    Thread seedthread = new Thread(() => { seed = SeedGen(); });
+                    seedthread.Start();
+                    Thread.Sleep(1);
+                    Thread multhread = new Thread(() => { multiplier = SeedGen(); });
+                    multhread.Start();
+                    Thread.Sleep(1);
+                    Thread modthread = new Thread(() => { modulo = SeedGen(); });
+                    modthread.Start();
+                    Thread.Sleep(1);
+                    Thread incthread = new Thread(() => { increment = SeedGen(); });
+                    incthread.Start();
+                    Thread.Sleep(1);
+                    TimeList = new List<double>();
+                    TimeList.Add(1);
+                    LastTime = DateTime.Now;
+                    seedthread.Join();
+                    multhread.Join();
+                    modthread.Join();
+                    incthread.Join();
+                }
+                if (type == 3)
+                {
+                    Seedlist = new List<double>();
+                    Thread[] threadarray = new Thread[75];
+                    for (int count = 0; count < 75; count++)
                     {
                         threadarray[count] = new Thread(() => { Seedlist.Add(SeedGen()); });
                         threadarray[count].Start();
                     }
-                    for (int count = 0; count < 50; count++)
+                    
+                    Thread Genthread = new Thread(() => { ListGenerator(); });
+                    Genthread.Start();
+                }
+                
+            }
+
+
+        }
+
+        void ListGenerator()
+        {
+            while (true)
+            {
+                Thread.Sleep(1);
+                if (Seedlist.Count < 300)
+                {
+                    Thread[] threadarray = new Thread[75];
+                    for (int count = 0; count < 75; count++)
+                    {
+                        threadarray[count] = new Thread(() => { Seedlist.Add(SeedGen()); });
+                        threadarray[count].Start();
+                    }
+                    for (int count = 0; count < 75; count++)
                     {
                         threadarray[count].Join();
                     }
-
-
                 }
             }
         }
-        long TimeAverage()
+        double TimeAverage()
         {
             DateTime temp = DateTime.Now;
-            long TimeDif = LastTime.Millisecond - temp.Millisecond;
+            double TimeDif = LastTime.Millisecond - temp.Millisecond;
             LastTime = temp;
             TimeList.Add(TimeDif);
-            long avg = 0;
-            for(int count = 0; count < TimeList.Count; count++)
+            double avg = 0;
+            for (int count = 0; count < TimeList.Count; count++)
             {
                 avg += TimeList[count] / TimeList.Count;
             }
-            while(TimeList.Count > 100)
+            while (TimeList.Count > 100)
             {
                 TimeList.RemoveAt(0);
             }
             return avg;
         }
 
-        public long getSeed()
+        public double getSeed()
         {
             return seed;
         }
-        public long getMult()
+        public double getMult()
         {
             return multiplier;
         }
 
-        public long getInc()
+        public double getInc()
         {
             return increment;
         }
 
-        public long getMod()
+        public double getMod()
         {
             return modulo;
         }
-        
-        public bool Eventgenerator(long freq) // using built in cryptographic random # generator to produce true returns 'freq' percentage of the time
+
+        public bool Eventgenerator(double freq) // using built in cryptographic random # generator to produce true returns 'freq' percentage of the time
         {
-            long randomnum = ReallyRandom();
+            double randomnum = ReallyRandom();
 
             if ((randomnum % 100) <= freq)
             {
@@ -270,27 +296,27 @@ namespace GenericTools
             }
         }
 
-        public long ReallyRandom(bool bypass=false)
+        public double ReallyRandom(bool bypass = false)
         {
             if (bypass == false)
             {
-                long waitingnum = ReallyRandom(true);
-                waitinglist.Add(waitingnum);
+                double waitingnum = this.ReallyRandom(true);
+                this.waitinglist.Add(waitingnum);
                 while (waitinglist[0] != waitingnum) // access control so multiple threads can't get the same value
                 {
                     Thread.Sleep(1);
                 }
             }
-                long randomnum = 1;
-            
-            if (Type == 0 | bypass ==true)
+            double randomnum = 1;
+
+            if (Type == 0 | bypass == true)
             {
                 RNGCryptoServiceProvider gibberish = new RNGCryptoServiceProvider();
                 byte[] buffer = new byte[8];
                 gibberish.GetBytes(buffer);
                 randomnum = BitConverter.ToInt64(buffer, 0);
             }
-            else if(Type == 1)
+            else if (Type == 1)
             {
                 randomnum = LinearDis();
             }
@@ -307,41 +333,41 @@ namespace GenericTools
             {
                 waitinglist.RemoveAt(0);
             }
-            return Convert.ToInt64(Math.Abs(randomnum));
+            return Math.Abs(randomnum);
         }
 
-        long LinearDis()
+        double LinearDis()
         {
 
-            
+
             seed = ((seed * multiplier) + increment) % modulo;
-            
+
             return seed;
         }
 
-        long ExpoDis()
+        double ExpoDis()
         {
-            
-            seed = Convert.ToInt64(-(1 / TimeAverage()) * Convert.ToInt64(Math.Log((Convert.ToDouble(LinearDis() / modulo))))); //some typecasting bullshittery is needed to  get the Log() function to play nice with long typecasting
-            
+
+            seed = (Math.Abs(-(1 / TimeAverage())  * Math.Log(LinearDis() / modulo))); //some typecasting bullshittery is needed to  get the Log() function to play nice with double typecasting
+
             return seed;
 
         }
 
-        long EntroDis()
+        double EntroDis()
         {
-            while(Seedlist.Count == 0)
+            while (Seedlist.Count == 0)
             {
                 Thread.Sleep(1);
             }
-            long temp = Seedlist[0];
+            double temp = Seedlist[0];
             Seedlist.RemoveAt(0);
             return temp;
         }
 
-        public long SeedGen() //generates a random long number
+        public double SeedGen() //generates a random double number
         {
-            long initialnum = 1;
+            double initialnum = 1;
             Ping Sender = new Ping();
             List<Thread> Pinglist = new List<Thread>();
             if (debug == true)
@@ -369,7 +395,7 @@ namespace GenericTools
                         if (reply.RoundtripTime != 0)
                         {
                             initialnum = initialnum * reply.RoundtripTime;
-                            
+
                         }
                         if (debug == true)
                         {
@@ -386,9 +412,9 @@ namespace GenericTools
                 });
                 Pinglist.Add(Pinger);
                 Pinger.Start();
-                Thread.Sleep(1);
+                
 
-                if (count%100 == 0)
+                if (count % 150 == 0)
                 {
                     if (debug == true)
                     {
@@ -398,26 +424,30 @@ namespace GenericTools
                     {
                         Pinglist[counter].Join();
                     }
+                    Pinglist.Clear();
                 }
                 count++;
-                
+
             }
-            
-            initialnum = initialnum % 4093082899; //large prime number close to the limit of an long
+            for (int counter = 0; counter < Pinglist.Count; counter++)
+            {
+                Pinglist[counter].Join();
+            }
+            initialnum = initialnum % 4093082899; //large prime number close to the limit of an double
             if (debug == true)
             {
                 Console.WriteLine("seed is " + initialnum);
             }
-            return Convert.ToInt64(initialnum);
+            return initialnum;
 
         }
 
         IPAddress ValidIP() // returns a valid non-reserved IP address
         {
-            long oct1 = (ReallyRandom(true) % 255);
-            long oct2 = (ReallyRandom(true) % 255);
-            long oct3 = (ReallyRandom(true) % 255);
-            long oct4 = (ReallyRandom(true) % 255);
+            double oct1 = (ReallyRandom(true) % 255);
+            double oct2 = (ReallyRandom(true) % 255);
+            double oct3 = (ReallyRandom(true) % 255);
+            double oct4 = (ReallyRandom(true) % 255);
             string IPstring;
             IPAddress newaddr;
 
